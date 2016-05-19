4d791182666f0be5fa6a88acad838091592b2996
baiou15 <ou.bai.dartmouth@gmail.com>
2016-05-19 17:17:05 -0400

    ShoppingList

---
+++ app/src/main/java/cs165/edu/dartmouth/cs/beforespoiled/SettingsFragment.java
@@ -0,0 +1,17 @@
+package cs165.edu.dartmouth.cs.beforespoiled;
+
+import android.app.Fragment;
+
+/**
+ * Created by oubai on 5/19/16.
+ */
+public class SettingsFragment extends Fragment {
+
+    public SettingsFragment() {
+
+    }
+
+    public static SettingsFragment newInstance() {
+        return new SettingsFragment();
+    }
+}
---
+++ app/src/main/java/cs165/edu/dartmouth/cs/beforespoiled/ShoppingListFragment.java
@@ -0,0 +1,17 @@
+package cs165.edu.dartmouth.cs.beforespoiled;
+
+import android.app.Fragment;
+
+/**
+ * Created by oubai on 5/19/16.
+ */
+public class ShoppingListFragment extends Fragment {
+
+    public ShoppingListFragment() {
+
+    }
+
+    public static ShoppingListFragment newInstance() {
+        return new ShoppingListFragment();
+    }
+}

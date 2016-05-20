6c45193f43f6836e1679992986dec87ad44c6a8d
cuteQ <cuteqianyuzhong@gmail.com>
2016-05-20 13:35:44 -0400

    ShoppingList

---
+++ app/src/main/java/cs165/edu/dartmouth/cs/beforespoiled/SaveShoppingItemToDatabase.java
@@ -0,0 +1,64 @@
+package cs165.edu.dartmouth.cs.beforespoiled;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by Yuzhong on 2016/4/19.
+ */
+public class SaveShoppingItemToDatabase extends AsyncTask<Void, Integer, Void> {
+    private ShoppingListDataSource mDataSource;
+    private ShoppingListItem shoppingListItem;
+    private ShoppingListItem newShoppingListItem;
+    private Context context;
+
+    public SaveShoppingItemToDatabase(Context context, ShoppingListItem shoppingListItem){
+        this.context = context;
+        this.shoppingListItem = shoppingListItem;
+    }
+    // A callback method executed on UI thread on starting the task
+    @Override
+    protected void onPreExecute() {
+        // Getting reference to the TextView tv_counter of the layout activity_main
+        mDataSource = new ShoppingListDataSource(context);
+    }
+
+    // A callback method executed on non UI thread, invoked after
+    // onPreExecute method if exists
+
+    // Takes a set of parameters of the type defined in your class implementation. This method will be
+    // executed on the background thread, so it must not attempt to interact with UI objects.
+    @Override
+    protected Void doInBackground(Void... params) {
+        mDataSource.open();
+        Log.d("Database", "Save data:" + shoppingListItem);
+        newShoppingListItem = mDataSource.createHistory(shoppingListItem);
+
+        mDataSource.close();
+        return null;
+    }
+
+    // A callback method executed on UI thread, invoked by the publishProgress()
+    // from doInBackground() method
+
+    // Overrider this handler to post interim updates to the UI thread. This handler receives the set of parameters
+    // passed in publishProgress from within doInbackground.
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        // Getting reference to the TextView tv_counter of the layout activity_main
+
+    }
+
+    // A callback method executed on UI thread, invoked after the completion of the task
+
+    // When doInbackground has completed, the return value from that method is passed into this event
+    // handler.
+    @Override
+    protected void onPostExecute(Void result) {
+        // Getting reference to the TextView tv_counter of the layout activity_main
+        Toast toast = Toast.makeText(context, "Entry #" + newShoppingListItem.getId() + "Saved", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+}

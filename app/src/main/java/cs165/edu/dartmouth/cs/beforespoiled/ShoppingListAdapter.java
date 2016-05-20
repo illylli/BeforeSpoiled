9a143e9db3033dae9721d1a30f56b787dcefd9aa
baiou15 <ou.bai.dartmouth@gmail.com>
2016-05-19 22:24:48 -0400

    add item, checkbox

--- app/src/main/java/cs165/edu/dartmouth/cs/beforespoiled/ShoppingListAdapter.java
+++ app/src/main/java/cs165/edu/dartmouth/cs/beforespoiled/ShoppingListAdapter.java
@@ -6,7 +6,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -20,7 +24,14 @@ public class ShoppingListAdapter extends ArrayAdapter<ShoppingListItem> {
     public ShoppingListAdapter(Context context, ArrayList<ShoppingListItem> shoppingListItems) {
         super(context, R.layout.shopping_list_item, shoppingListItems);
         this.context = context;
-        this.shoppingListItems = shoppingListItems;
+//        this.shoppingListItems = shoppingListItems;
+        this.shoppingListItems = new ArrayList<ShoppingListItem>();
+        this.shoppingListItems.addAll(shoppingListItems);
+    }
+
+    private class ViewHolder {
+        TextView itemName;
+        CheckBox ifBought;
     }
 
     @Override
@@ -40,33 +51,61 @@ public class ShoppingListAdapter extends ArrayAdapter<ShoppingListItem> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        View twoLineListItem;
-        if (convertView == null) {
-            LayoutInflater inflater = (LayoutInflater) context
-                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            twoLineListItem = inflater.inflate(
-                    R.layout.shopping_list_item, null);
+        ViewHolder holder = null;
+        if(convertView == null) {
+            LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.shopping_list_item, null);
+
+            holder = new ViewHolder();
+            holder.itemName = (TextView) convertView.findViewById(R.id.item_name);
+            holder.ifBought = (CheckBox) convertView.findViewById(R.id.check_box);
+            convertView.setTag(holder);
+
+            holder.itemName.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    CheckBox checkBox = (CheckBox) v;
+                    ShoppingListItem listItem = (ShoppingListItem) checkBox.getTag();
+                    Toast.makeText(getContext(), "Clicked on Checkbox" + checkBox.isChecked(), Toast.LENGTH_LONG).show();
+                    listItem.setSelected(checkBox.isChecked());
+                }
+            });
         } else {
-            twoLineListItem = convertView;
+            holder = (ViewHolder) convertView.getTag();
         }
 
-        TextView text1 = (TextView) twoLineListItem.findViewById(R.id.item_name);
-        TextView text2 = (TextView) twoLineListItem.findViewById(R.id.item_number);
+        ShoppingListItem listItem = shoppingListItems.get(position);
+        holder.itemName.setText(listItem.getItemName());
+        holder.ifBought.setChecked(listItem.isSelected());
 
-        text1.setTextColor(Color.GRAY);
-        text2.setTextColor(Color.GRAY);
-        text1.setText(getFirstRow(shoppingListItems.get(position)));
-        text2.setText(getSecondRow(shoppingListItems.get(position)));
-
-        return twoLineListItem;
+        return convertView;
+//        View twoLineListItem;
+//        if (convertView == null) {
+//            LayoutInflater inflater = (LayoutInflater) context
+//                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//            twoLineListItem = inflater.inflate(
+//                    R.layout.shopping_list_item, null);
+//        } else {
+//            twoLineListItem = convertView;
+//        }
+//
+//        TextView text1 = (TextView) twoLineListItem.findViewById(R.id.item_name);
+//        TextView text2 = (TextView) twoLineListItem.findViewById(R.id.item_number);
+//
+//        text1.setTextColor(Color.GRAY);
+//        text2.setTextColor(Color.GRAY);
+//        text1.setText(getFirstRow(shoppingListItems.get(position)));
+//        text2.setText(getSecondRow(shoppingListItems.get(position)));
+//
+//        return twoLineListItem;
     }
+
     //Show the first row in history entry list.
-    public String getFirstRow(ShoppingListItem shoppingListItem){
-        return "Row1";
-    }
+//    public String getFirstRow(ShoppingListItem shoppingListItem){
+//        return "Row1";
+//    }
     //Show the second row in history entry list.
-    public String getSecondRow(ShoppingListItem shoppingListItem){
-        return "Row2";
-    }
+//    public String getSecondRow(ShoppingListItem shoppingListItem){
+//        return "Row2";
+//    }
 
 }
